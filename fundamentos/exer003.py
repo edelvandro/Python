9dc945ece7b23eca38abee8113d9d6b0a516ce45
edelvandro <edelvandro@gmail.com>
2020-04-14 10:50:20 -0300

    Soma entre dois números

---
+++ fundamentos/exer003.py
@@ -0,0 +1,7 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+
+num1 = int(input('Digite o primeiro valor: '))
+num2 = int(input('Digite o segundo valor: '))
+soma = num1 + num2
+print('O resultado da soma entre {} e {} é: {}'.format(num1, num2, soma))
