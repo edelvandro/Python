0ca452564a67667d9770186d48f7b2a01f04f422
edelvandro <edelvandro@gmail.com>
2020-04-14 11:04:18 -0300

    Tabuada de um número

---
+++ fundamentos/exer009.py
@@ -0,0 +1,16 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada.
+
+num = int(input('Digite um número: '))
+print("_" * 20)
+print('{} * {:2} = {}'.format(num, 0, (num * 0)))  # {:2} tabulação
+print('{} * {:2} = {}'.format(num, 1, (num * 1)))
+print('{} * {:2} = {}'.format(num, 2, (num * 2)))
+print('{} * {:2} = {}'.format(num, 3, (num * 3)))
+print('{} * {:2} = {}'.format(num, 4, (num * 4)))
+print('{} * {:2} = {}'.format(num, 5, (num * 5)))
+print('{} * {:2} = {}'.format(num, 6, (num * 6)))
+print('{} * {:2} = {}'.format(num, 7, (num * 7)))
+print('{} * {:2} = {}'.format(num, 8, (num * 8)))
+print('{} * {:2} = {}'.format(num, 9, (num * 9)))
+print('{} * {:2} = {}'.format(num, 10, (num * 10)))
+print('_' * 20)
